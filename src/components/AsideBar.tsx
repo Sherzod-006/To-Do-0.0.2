cf122e7cd182696a4a0a64c325b3fee7c34871e8
Sherzod Sobirov <sherzodsobirovv1@gmail.com>
2025-08-22 22:48:29 +0500

    updated router links

--- src/components/AsideBar.tsx
+++ src/components/AsideBar.tsx
@@ -1,14 +1,16 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHouse, faPlus, faBorderAll, faUser } from '@fortawesome/free-solid-svg-icons';
 import type { IconProp } from '@fortawesome/fontawesome-svg-core';
 
 const AsideBar = () => {
+  const location = useLocation();
 
-    //STYLING
-    const linkClasses: string = `block w-full py-2 px-3 font-bold text-blue-950 hover:bg-blue-100 hover:rounded-xl`;
-    const aside: string = "fixed top-0 left-0 bg-white w-64 min-h-screen shadow-2xl p-4"
-    const title: string = "text-3xl font-bold font-serif mt-5 mb-10 text-blue-950"
+  //STYLING
+  const linkClasses: string = `block w-full my-1 py-2 px-3 font-bold text-blue-950 hover:bg-blue-100 rounded-xl`;
+  const aside: string = "fixed top-0 left-0 bg-white w-64 min-h-screen shadow-2xl p-4"
+  const title: string = "text-3xl font-bold font-serif mt-5 mb-10 text-blue-950"
 
     //TEMPLATES
     function renderLink(className: string, href: string, value: string, icons: IconProp){
@@ -21,13 +23,13 @@ const AsideBar = () => {
 
   return (
     <aside className={aside}>
-      <h1 className={title}>Task Manager</h1>
+      <h1 className={title}><Link to="/">Task Manager</Link></h1>
       <nav>
         <ul>
-            <li>{renderLink(linkClasses, "/", "Dashboard", faHouse)}</li>
-            <li>{renderLink(linkClasses, "/add-task", "Add Task", faPlus)}</li>
-            <li>{renderLink(linkClasses, "/all", "All", faBorderAll)}</li>
-            <li>{renderLink(linkClasses, "/profile", "Profile", faUser)}</li>
+            <li>{renderLink(location.pathname === "/" ? `${linkClasses} bg-blue-100` : linkClasses, "/",        "Dashboard", faHouse)}</li>
+            <li>{renderLink(location.pathname === "/add-task" ? `${linkClasses} bg-blue-100` : linkClasses, "/add-task", "Add Task", faPlus)}</li>
+            <li>{renderLink(location.pathname === "/all" ? `${linkClasses} bg-blue-100` : linkClasses, "/all", "All", faBorderAll)}</li>
+            <li>{renderLink(location.pathname === "/profile" ? `${linkClasses} bg-blue-100` : linkClasses, "/profile", "Profile", faUser)}</li>
         </ul>
       </nav>
     </aside>
